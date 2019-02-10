fc6e6e78b6cfb81a5ed2d3aad2b9b7d93c554640
JiaruiJin <43017807+JiaruiJin@users.noreply.github.com>
2019-02-10 11:24:34 -0500

    Create DatabaseManager.py

---
+++ DatabaseManager.py
@@ -0,0 +1,27 @@
+class Databasemanager():
+    '''
+    :param time: uint32_t value of time;
+    :param data: uint32_t value of blood oxygen level;
+    returns: void function ( update database )
+    :param time: uint32_t value of time;
+    :param data: uint32_t value of blood pressure level;
+    returns: void function ( update database )
+    :param time: uint32_t value of time;
+    :param data: uint32_t value of blood pulse level;
+    returns: void function ( update database )
+    :param start_time: uint32_t value of start time;
+    :param end_time: uint32_t value of end time;
+    returns: blood oxygen data in the time period ( database query )
+    :param start_time: uint32_t value of start time;
+    :param end_time: uint32_t value of end time;
+    returns: blood pressure data in the time period ( database query )
+    :param start_time: uint32_t value of start time;
+    :param end_time: uint32_t value of end time;
+    returns: blood pulse data in the time period ( database query )
+    '''
+    def update_oxygen(double time, double data):
+    def update_pressure(double time, double data):
+    def update_pulse(double time, double data):
+    def query_oxygen(double start_time, double end_time):
+    def query_pressure(double start_time, double end_time):
+    def query_pulse(double start_time, double end_time):
